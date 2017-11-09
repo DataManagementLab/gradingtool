ce7ce92eba36d700b9d00fda687d85bf878a3bd8
Benjamin Haettasch <h@ttas.ch>
2017-11-09 14:37:51 +0100

    Ported to python3

--- autograde.py
+++ autograde.py
@@ -49,8 +49,6 @@ exercise_folder = args.exercise[0]
 submission_folder = args.submissions[0]
 output_folder = args.output[0]
 
-reload(sys)
-sys.setdefaultencoding('utf8')
 
 # Initilaize data structure
 results = None
@@ -71,26 +69,26 @@ rootPath = submission_folder
 pattern = '*.zip'
 for root, dirs, files in os.walk(rootPath):
     for filename in fnmatch.filter(files, pattern):
-	f_path = os.path.join(root, filename)
-	temp_index = f_path.find('_assignsubmission')
-	tem_str = f_path[:temp_index]
-	print(tem_str)
+        f_path = os.path.join(root, filename)
+        temp_index = f_path.find('_assignsubmission')
+        tem_str = f_path[:temp_index]
+        print(tem_str)
         #print(os.path.join(root, os.path.splitext(filename)[0]))
         #zipfile.ZipFile(os.path.join(root, filename)).extractall(os.path.join(root, tem_str))
-	zipfile.ZipFile(os.path.join(root, filename)).extractall(tem_str)
+        zipfile.ZipFile(os.path.join(root, filename)).extractall(tem_str)
 
 pattern = '*.rar'
 for root, dirs, files in os.walk(rootPath):
     for filename in fnmatch.filter(files, pattern):
-	tf_path = os.path.join(root, filename)
-	temp_index = f_path.find('_assignsubmission')
-	tem_str = f_path[:temp_index]
-	print(os.path.join(root, tem_str))
-	print("raaaaaaaaaaaaaaaar:"+tem_str)
-	print(os.path.join(root, tem_str))
-	if not os.path.exists(os.path.join(root, tem_str)):
-    		os.makedirs(os.path.join(root, tem_str))
-	rf = rarfile.RarFile(os.path.join(root, filename))
+        tf_path = os.path.join(root, filename)
+        temp_index = f_path.find('_assignsubmission')
+        tem_str = f_path[:temp_index]
+        print(os.path.join(root, tem_str))
+        print("raaaaaaaaaaaaaaaar:"+tem_str)
+        print(os.path.join(root, tem_str))
+        if not os.path.exists(os.path.join(root, tem_str)):
+                os.makedirs(os.path.join(root, tem_str))
+        rf = rarfile.RarFile(os.path.join(root, filename))
         rf.extract(os.path.join(root, tem_str))
 
 # TODO Implement, insert records for every group in result structure ??
@@ -121,56 +119,56 @@ writer = csv.writer(f_writ, delimiter=',',
                 quotechar = "'"
                 )
 with open(input_file_name, 'r') as csvfile:
-	reader = csv.DictReader(csvfile, fieldnames=fieldnames)
-	for row in reader:
-		folder_name = row["Group"]+"-"+row["Full name"]+"_"+row["Identifier"][12:]
-		print((os.path.join(submission_folder,folder_name)))
-		print(os.path.isdir(os.path.join(submission_folder,folder_name)))		
-		if os.path.isdir(os.path.join(submission_folder,folder_name)):
-			print(folder_name+"	"+row["Identifier"])
-			total_mark=0
-			total_comment=''
-			for i in config_data:
-				print i['params']
-				#print("evaluators/" + i['evaluator'])
-				#print("input/" + folder_name)
-				p,c = run_evaluation(exercise_folder + i['evaluator'],input_folder_path + folder_name,"exercise",i['params'])
-				total_mark+=p
-				total_comment+=c
-			print(total_mark,total_comment)
-			row["Grade"]=total_mark
-			row["Feedback comments"]=total_comment
-			writer.writerow(['\"'+row["Identifier"]+'\"','\"'+row["Full name"]+'\"',row["Email address"],'\"'+row["Status"]+'\"','\"'+row["Group"]+'\"',row["Grade"],row["Maximum Grade"],row["Grade can be changed"],'\"'+row["Last modified (submission)"]+'\"',row["Feedback comments"]])
-	
+    reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+    for row in reader:
+        folder_name = row["Group"]+"-"+row["Full name"]+"_"+row["Identifier"][12:]
+        print((os.path.join(submission_folder,folder_name)))
+        print(os.path.isdir(os.path.join(submission_folder,folder_name)))
+        if os.path.isdir(os.path.join(submission_folder,folder_name)):
+            print(folder_name+"	"+row["Identifier"])
+            total_mark=0
+            total_comment=''
+            for i in config_data:
+                print(i['params'])
+                #print("evaluators/" + i['evaluator'])
+                #print("input/" + folder_name)
+                p,c = run_evaluation(exercise_folder + i['evaluator'],input_folder_path + folder_name,"exercise",i['params'])
+                total_mark+=p
+                total_comment+=c
+            print(total_mark,total_comment)
+            row["Grade"]=total_mark
+            row["Feedback comments"]=total_comment
+            writer.writerow(['\"'+row["Identifier"]+'\"','\"'+row["Full name"]+'\"',row["Email address"],'\"'+row["Status"]+'\"','\"'+row["Group"]+'\"',row["Grade"],row["Maximum Grade"],row["Grade can be changed"],'\"'+row["Last modified (submission)"]+'\"',row["Feedback comments"]])
 
-	"""
-	for i in config_data:
-		for dr in dirs:
-			matchObj = re.match( r'Gruppe (.*)-(.*)_(.*)', dr, re.M|re.I)
-			test2 = matchObj.group(2)
-			mrt_i =test2.find("_")
-			Participant_id=test2[mrt_i+1:]
-			Participant_id="Participant "+Participant_id[:Participant_id.find("_")]
-			#print(Participant_id)
-			with open(input_file_name, 'r') as csvfile:
-				reader = csv.DictReader(csvfile, fieldnames=fieldnames)
-				for row in reader:
-					#print("row_var:"+row["Identifier"])
-					if Participant_id == row["Identifier"]:
-						print i['name']
-						#print(row)
-						p,c = run_evaluation("evaluators/line_by_line.py","input/Gruppe 14-assignment submission_17889651","exercise",{'filename': 'answer_1.txt', 'reference': 'reference_1.txt'})
-						#print(p,c)
-						
+
+    """
+    for i in config_data:
+        for dr in dirs:
+            matchObj = re.match( r'Gruppe (.*)-(.*)_(.*)', dr, re.M|re.I)
+            test2 = matchObj.group(2)
+            mrt_i =test2.find("_")
+            Participant_id=test2[mrt_i+1:]
+            Participant_id="Participant "+Participant_id[:Participant_id.find("_")]
+            #print(Participant_id)
+            with open(input_file_name, 'r') as csvfile:
+                reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+                for row in reader:
+                    #print("row_var:"+row["Identifier"])
+                    if Participant_id == row["Identifier"]:
+                        print i['name']
+                        #print(row)
+                        p,c = run_evaluation("evaluators/line_by_line.py","input/Gruppe 14-assignment submission_17889651","exercise",{'filename': 'answer_1.txt', 'reference': 'reference_1.txt'})
+                        #print(p,c)
+                        
 """
 
-						
+
 
 """
-	for row in reader:
-		if name == row["Identifier"]:
-			print(row)
-			writer.writerow(['\"'+row["Identifier"]+'\"','\"'+row["Full name"]+'\"',row["Email address"],'\"'+row["Status"]+'\"','\"'+row["Group"]+'\"',row["Grade"],row["Maximum Grade"],row["Grade can be changed"],'\"'+row["Last modified (submission)"]+'\"',row["Feedback comments"]])
+    for row in reader:
+        if name == row["Identifier"]:
+            print(row)
+            writer.writerow(['\"'+row["Identifier"]+'\"','\"'+row["Full name"]+'\"',row["Email address"],'\"'+row["Status"]+'\"','\"'+row["Group"]+'\"',row["Grade"],row["Maximum Grade"],row["Grade can be changed"],'\"'+row["Last modified (submission)"]+'\"',row["Feedback comments"]])
 
 
 f_writ.close()"""
@@ -178,11 +176,11 @@ f_writ.close()"""
 """
 for i in config_data:
 #    print i['name']
-	for dr in dirs:
-		if(os.path.isfile(iput_abs_path + "/" + dr + "/"+i['params']["filename"])):
-			#print("evaluators/" + i['evaluator'],"input/" + dr,"exercise",i['params'], dr[:10],iput_abs_path + "/" + dr + "/"+i['params']["filename"])
-			results[results.index([dr[:10]])].append([run_evaluation("evaluators/" + i['evaluator'],"input/" + dr,"exercise",i['params'])])
-			#print(dr[:10],results.index([dr[:10]]))
+    for dr in dirs:
+        if(os.path.isfile(iput_abs_path + "/" + dr + "/"+i['params']["filename"])):
+            #print("evaluators/" + i['evaluator'],"input/" + dr,"exercise",i['params'], dr[:10],iput_abs_path + "/" + dr + "/"+i['params']["filename"])
+            results[results.index([dr[:10]])].append([run_evaluation("evaluators/" + i['evaluator'],"input/" + dr,"exercise",i['params'])])
+            #print(dr[:10],results.index([dr[:10]]))
 """
 
 """
