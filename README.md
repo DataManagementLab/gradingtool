3d54c35aca87436a3ce8cee6c9b06b069f2d3297
Benjamin Haettasch <benjamin.haettasch@googlemail.com>
2017-11-13 00:48:03 +0100

    Implemented export to moodle
    Also added usage (command line interface) to README

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 Python Framework to grade assignment submissions
 
 ## usage
+
+### autograding tool
     autograde.py [-h] [--plagcheck] exercise submissions output
 
     positional arguments:
@@ -12,3 +14,15 @@ Python Framework to grade assignment submissions
     optional arguments:
       -h, --help	show this help message and exit
       --plagcheck	Perform check for plagiarism
+
+
+### moodle export
+    export_to_moodle.py [-h] gradingsheet results passing_threshold
+
+    positional arguments:
+      gradingsheet       stencil file to fill with results
+      results            folder containing the results
+      passing_threshold  threshold to pass the exercise (between 0 and 1.0)
+
+    optional arguments:
+      -h, --help         show this help message and exit
--- export_to_moodle.py
+++ export_to_moodle.py
@@ -1,6 +1,12 @@
 import argparse
 import csv
+import json
 import os
+import locale
+import datetime
+
+locale.setlocale(locale.LC_ALL, 'DE')
+timestamp = f"{datetime.datetime.now():%A, %d %B %Y, %H:%M}"
 
 
 # Command line interface
@@ -9,26 +15,73 @@ parser.add_argument('gradingsheet', metavar='gradingsheet', type=str, nargs=1,
                     help='stencil file to fill with results')
 parser.add_argument('results', metavar='results', type=str, nargs=1,
                     help='folder containing the results')
-parser.add_argument('results', metavar='results', type=str, nargs=1,
-                    help='folder containing the results')
-
+parser.add_argument('passing_threshold', metavar='passing_threshold', type=float, nargs=1,
+                    help='threshold to pass the exercise (between 0 and 1.0)')
 args = parser.parse_args()
 
 gradingsheet = args.gradingsheet[0]
 results_folder = args.results[0]
+passing_threshold = args.passing_threshold[0]
 
 
+# Load gardingsheet (stencil)
 moodle_import_file = open(os.path.join(results_folder, "grades.csv"), 'w')
-fieldnames = ["Identifier", "Full name","Email address", "Status","Group","Grade","Maximum Grade","Grade can be changed","Last modified (submission)","Last modified (grade)","Feedback comments"]
-writer = csv.writer(moodle_import_file, delimiter=',',
-                    lineterminator='\r\n',
-                    quotechar = "'"
-                    )
+fieldnames = ["Identifier", "Full name", "Email address", "Status", "Group", "Grade", "Maximum Grade",
+              "Grade can be changed", "Last modified (submission)", "Last modified (grade)", "Feedback comments"]
 
+
+# Prepare writer and write headlines
+writer = csv.writer(moodle_import_file, delimiter=',', quotechar='"', lineterminator='\n')
+writer.writerow(fieldnames)
+
+
+# Open result file
 with open(gradingsheet, 'r') as csvfile:
+    # Read gradingsheet line by line (ignore first line)
     reader = csv.DictReader(csvfile, fieldnames=fieldnames)
-    for row in reader:
-            row["Grade"]=0
-            row["Feedback comments"]=""
-            writer.writerow(['\"'+row["Identifier"]+'\"','\"'+row["Full name"]+'\"',row["Email address"],'\"'+row["Status"]+'\"','\"'+row["Group"]+'\"',row["Grade"],row["Maximum Grade"],row["Grade can be changed"],'\"'+row["Last modified (submission)"]+'\"',row["Feedback comments"]])
+    next(reader)
 
+    # For all entries (rows) in csv...
+    for row in reader:
+        # ...check if there is a submission
+        if not row["Status"].startswith("No submission"):
+            # Yes? Calculate grade and create comment
+
+            # Open results of submission
+            results_file = os.path.join(results_folder, row["Group"], "results.json")
+            results = json.load(open(results_file, "r"))
+
+            # Calculate grade
+            points = sum(r["points"] for r in results)
+            total_points = sum(r["total_points"] for r in results)
+            passed = points / total_points >= passing_threshold
+            grade = 1 if passed else 0
+
+            # Combine comment
+            comment_table  = "<br>".join(f"{r['name']}: {r['points']}/{r['total_points']}" for r in results)
+            comment_details = "<br>".join(f"{r['name']}: {r['comment']}" for r in results if r['comment'] != "")
+            comment = comment_table + "<br><br>" + comment_details
+
+            # Set current timestamp
+            outputtimestamp = timestamp
+        else:
+            # No? Output default values
+            grade = 0
+            comment = ""
+            outputtimestamp = "-"
+
+        # Create and write output line
+        outputrow = [
+            row["Identifier"],
+            row["Full name"],
+            row["Email address"],
+            row["Status"],
+            row["Group"],
+            grade,
+            row["Maximum Grade"],
+            row["Grade can be changed"],
+            row["Last modified (submission)"],
+            outputtimestamp,
+            comment
+        ]
+        writer.writerow(outputrow)
