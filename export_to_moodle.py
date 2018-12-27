3a886718aa71bab76416237027a46485a452802f
mjasny <matthiaschristian.jasny@stud.tu-darmstadt.de>
2018-12-27 13:28:56 +0100

    implement plagiarism -> not passed. e.g. --no-pass 34,32,34,14

--- export_to_moodle.py
+++ export_to_moodle.py
@@ -28,11 +28,17 @@ parser.add_argument('results', metavar='results', type=str, nargs=1,
                     help='folder containing the results')
 parser.add_argument('passing_threshold', metavar='passing_threshold', type=float, nargs=1,
                     help='threshold to pass the exercise (between 0 and 1.0)')
+parser.add_argument('--no-pass', metavar='no_pass', type=str, nargs=1,
+                    help='specify group numbers that should not pass, comma separated')
 args = parser.parse_args()
 
 gradingsheet = args.gradingsheet[0]
 results_folder = args.results[0]
 passing_threshold = args.passing_threshold[0]
+
+shall_not_pass = set()
+if args.no_pass:
+    shall_not_pass.update(args.no_pass[0].split(','))
 
 
 # Load gardingsheet (stencil)
@@ -72,6 +78,10 @@ with open(gradingsheet, 'r') as csvfile:
                 continue
             results = json.load(open(results_file, "r"))
 
+            plagiarism = False
+            if shall_not_pass and row['Group'].endswith(tuple(shall_not_pass)):
+                plagiarism = True
+
             # Calculate grade
             names = [r["name"] for r in results]
             points = [r["points"] if r["points"] > 0 else 0 for r in results]
@@ -81,6 +91,8 @@ with open(gradingsheet, 'r') as csvfile:
             points_scored = sum(points)
             total_points_possible = sum(total_points)
             passed = points_scored / total_points_possible >= passing_threshold
+            if plagiarism:
+                passed = False
             grade = 1 if passed else 0
 
             # Combine comment
@@ -90,6 +102,8 @@ with open(gradingsheet, 'r') as csvfile:
                 comment += f"<tr><td valign=\"top\">{n}</td><td valign=\"top\" align=\"right\">{p}/{tp}</td><td>{c}</td></tr>"
             comment += "</table>"
             comment += f"<br><br>{points_scored}/{total_points_possible} => Passed: {passed}"
+            if plagiarism:
+                comment += '<br>br>You did not pass because plagiarism was detected.'
 
             # Set current timestamp
             outputtimestamp = timestamp
